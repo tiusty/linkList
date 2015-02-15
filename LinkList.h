38f267432e892b31cbd4edb34f60614336557d06
tiusty <awagud@hotmail.com>
2015-02-15 13:45:51 -0500

    Create LinkList.h

---
+++ LinkList.h
@@ -0,0 +1,36 @@
+/*
+ * LinkList.h
+ *
+ *  Created on: Feb 15, 2015
+ *      Author: alex
+ */
+
+#ifndef LINKLIST_H_
+#define LINKLIST_H_
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//creates a structure of sName
+struct sName
+{
+	string key;
+	sName *pNext;
+};
+
+class LinkList {
+private:
+	sName *head;
+	int length;
+public:
+	LinkList();
+	void insertItem (sName * newItem);
+	bool removeItem (string itemKey);
+	sName * getItem (string itemKey);
+	void printList();
+	int getLength();
+	virtual ~LinkList();
+};
+
+#endif /* LINKLIST_H_ */
